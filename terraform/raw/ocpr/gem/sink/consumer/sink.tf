6b60f991e14092a6fa46221d5ce7e5cee0043229
shr2911 <68686711+shr2911@users.noreply.github.com>
2020-07-24 21:58:38 +0530

    Delete sink.tf

--- terraform/raw/ocpr/gem/sink/consumer/sink.tf
+++
@@ -1,6 +0,0 @@
-module "customer-gem-sink" {
-source                   = "../../../../../modules/gem/raw/sink/consumer"
-subject_area              = var.app_configs["subject_area"]
-domain_object_type       = var.app_configs["domain_object_type"]
-stage                    = var.stage
-}
