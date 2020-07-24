3a01958353c0049c9a3487501563e65b528b186d
shr2911 <68686711+shr2911@users.noreply.github.com>
2020-07-24 21:59:24 +0530

    Update source.tf

--- terraform/curated/ocpr_cdc/gem/source.tf
+++ terraform/curated/ocpr_cdc/gem/source.tf
@@ -1,7 +1,1 @@
-module "customer-gem-source" {
-  source                   = "../../../modules/gem/curated/source"
-  sub_project              = var.app_configs["sub_project"]
-  domain_fullname          = var.app_configs["domain_fullname"]
-  domain_object_type       = var.app_configs["domain_object_type"]
-  stage                    = var.stage
-}
+"ddd"
